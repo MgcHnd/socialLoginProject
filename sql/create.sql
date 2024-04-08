36552944d2ae2b15c5f7ad0f1ee1fb713867547d
MgcHnd <mgchnd22@gmail.com>
2024-04-08 18:22:37 +0900

    Update create.sql

--- sql/create.sql
+++ sql/create.sql
@@ -2,39 +2,20 @@ DROP SCHEMA IF EXISTS socialLoginProject;
 CREATE SCHEMA IF NOT EXISTS socialLoginProject DEFAULT CHARACTER SET utf8;
 USE socialLoginProject;
 
-DROP TABLE IF EXISTS socialLoginProject.UserInfos;
+DROP TABLE IF EXISTS socialLoginProject.user;
 
-CREATE TABLE UserInfos (
-  userId VARCHAR(60) PRIMARY KEY, -- What does this do?
-  username VARCHAR(20),
-  profileImg VARCHAR(200),
-  created DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-  updated DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP()
-)
+CREATE TABLE user (
+	userId VARCHAR(60),
+    userName VARCHAR(20),
+    profileImg VARCHAR(200),
+    created DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    updated DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
+    PRIMARY KEY(userId)
+);
 
-INSERT INTO UserInfos (userId, username, profileImg) VALUES
-('user1', 'Dexter', '/IMG/profile1.jpg'),
-('user2', 'Sophie', '/IMG/profile2.jpg'),
-('user3', 'Aiden', '/IMG/profile3.jpg'),
-('user4', 'Ethan', '/IMG/profile4.jpg'),
-('user5', 'Colin', '/IMG/profile5.jpg');
-
--- Query 1
-SELECT userId, username
-FROM UserInfos
-ORDER BY created DESC
-LIMIT 1;
-
--- Query 2
-SELECT username
-FROM UserInfos
-WHERE userId = 'asd';
-
--- Query 3
-DELETE FROM UserInfos
-WHERE username = '박근원';
-
--- Query 4
-UPDATE UserInfos
-SET userId = 'dsa'
-WHERE username = '김기재';
+INSERT INTO user (userId, userName, profileImg) VALUES
+("root", "근원", "asdfgh"),
+("park", "공원", "qwerty"),
+("rlwo", "기재", "zxcvbn"),
+("xodn", "태우", "hjklop"),
+("woals", "재민", "tyuio");
